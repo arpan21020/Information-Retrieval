4dbb0e4e88e5d66f9bd2a8508a2f115c45dc12eb
arpan21020 <arpan21020@iiitd.ac.in>
2024-04-24 01:24:24 +0530

    assignment done

--- Mid_TermProjectReview/chatbotui/chatbotui/myapp/views.py
+++ Mid_TermProjectReview/chatbotui/chatbotui/myapp/views.py
@@ -1,46 +1,64 @@
 # myapp/views.py
 from django.shortcuts import render
-from transformers import PreTrainedTokenizerFast, GPT2LMHeadModel, GPT2TokenizerFast, GPT2Tokenizer
+# from transformers import PreTrainedTokenizerFast, GPT1LMHeadModel, GPT2TokenizerFast, GPT2Tokenizer
 import os
+from langchain.chains import create_retrieval_chain
+import pickle
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_history_aware_retriever
+from langchain_core.prompts import MessagesPlaceholder
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.llms import Ollama
+
+llm = Ollama(model="llama3")
 
 def my_view(request):
     output_str = ''
     if request.method == 'POST':
         input_str = request.POST.get('input_str', '')
-        output_str = generate_text(input_str, 200)
+        output_str = generate_text(input_str)
     return render(request, 'index.html', {'output_str': output_str})
 
+def generate_text(user_query):
+    
+    with open("/home/najiya/Desktop/Arpan/IR_Project/compress_file_chatbot/myapp/static/vector", "rb") as f:
+        vector = pickle.load(f)
 
-def load_model(model_path):
-    model = GPT2LMHeadModel.from_pretrained(model_path)
-    return model
+    retriever = vector.as_retriever()
+
+    # retriever = vector.as_retriever()
+
+    # First we need a prompt that we can pass into an LLM to generate this search query
+
+    prompt = ChatPromptTemplate.from_template(""" 
+        A document is provided for some schemes
+                                            by the states of Delhi, UP, Haryana, Rajasthan, Maharashtra
+                                            and some schemes by the Central Government. 
+                                            Take information from the document and answer
+                                            people's questions. Search the document first.
+                                            If you can't find relevant data, look it up.
+        We have sentiment analysed social media posts in the document too.
+        A post's rating of 0 means negative, 1 means neutral and 2 means positive sentiments.
+        You have to perform data analysis on the documents using these as requested. Give a brief
+        view about people's opinions on a scheme if asked. If there are profane words, ignore those
+        posts.
+        Given a user conversation, search the document first to
+        get information relevant to the conversation. If nothing is found, look it up.
+
+    <context>
+    {context}
+    </context>
+
+    Question: {input}""")
+
+    document_chain = create_stuff_documents_chain(llm, prompt)
+    # retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+
+    retrieval_chain = create_retrieval_chain(retriever, document_chain)
 
 
-def load_tokenizer(tokenizer_path):
-    tokenizer = GPT2Tokenizer.from_pretrained(tokenizer_path)
-    return tokenizer
+    response = retrieval_chain.invoke({"input": """" {user_query}"""})
+    print(response['answer'])
 
-
-def generate_text(sequence, max_length):
-    model_path = "D:\IR_project\Information-Retrieval\model_output"
-    print("Hello", os.path.exists(path= model_path))
-    "chatbotui\myapp\views.py"
-    model = load_model(model_path)
-    tokenizer = load_tokenizer(model_path)
-    ids = tokenizer.encode(f'{sequence}', return_tensors='pt')
-    final_outputs = model.generate(
-        ids,
-        do_sample=True,
-        max_length=max_length,
-        pad_token_id=model.config.eos_token_id,
-        top_k=50,
-        top_p=0.95,
-    )
-    # print("Chatbot: ")
-    # print(tokenizer.decode(final_outputs[0], skip_special_tokens=True))
-    return tokenizer.decode(final_outputs[0], skip_special_tokens=True)
-
-
-# sequence = input("User: ")
-# max_len = 150
-# generate_text(sequence, max_len)
+    return response['answer']
